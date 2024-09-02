fb395aa11c0369482fd0bc02733dc4a3950f8e4c
caiovlima <caio_lima_nh@hotmail.com>
2024-09-02 00:29:22 -0300

    :lipstick: add: pipe para transformar nomes

---
+++ src/app/shared/utils/pipes/util.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { UtilPipe } from './util.pipe';
+
+describe('UtilPipe', () => {
+  it('create an instance', () => {
+    const pipe = new UtilPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/shared/utils/services/util.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { InfoModalComponent } from '../../componentes/info-modal/info-modal.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+  constructor(private dialog: MatDialog) { }
+
+  openInfoDialog(): void {
+    this.dialog.open(InfoModalComponent);
+  }
+
+  closeDialog(): void {
+    this.dialog.closeAll();
+  }
+}
